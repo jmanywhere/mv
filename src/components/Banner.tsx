0ea11499ba5bf8acef580facc47bbdf79ffec06d
Semi <>
2022-12-21 19:03:27 -0600

    fix horizontal scroll show

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,36 +4,12 @@ import Image from "next/image";
 const Banner = () => {
   return (
     <section
-      className="
-                flex
-                w-full
-                flex-col
-                items-center
-                
-                bg-black
-                pt-[191px]
-                pb-[56px]
-                lg:rounded-br-[150px]
-                lg:pt-[430px]
-                lg:pb-[108px]
-                " //bg-[url('./../../public/images/FONDO BANNER')] para poner la imagen y quitar bg-black
+      className=" bg-black pb-[56px] lg:rounded-br-[90px]" //bg-[url('./../../public/images/FONDO BANNER')] para poner la imagen y quitar bg-black
     >
-      <h2
-        className=" 
-                    text-center
-                    text-[34px]
-                    text-white
-                    
-      "
-      >
-        Every <span className="text-[#2192dd]">Moon</span> has start Somewhere
+      <h2 className="pt-[191px] text-center text-4xl text-white">
+        Every <span className="text-primary">Moon</span> has start Somewhere
       </h2>
-      <h3
-        className=" 
-                  text-lg
-                    text-white    
-      "
-      >
+      <h3 className="text-center text-lg text-white">
         Crowd funding made Easy
       </h3>
     </section>
