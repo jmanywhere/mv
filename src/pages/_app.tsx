e5f0050d69d2ee2632da444c7ea7de3f4f98731b
Semi <>
2022-12-28 00:25:42 -0600

    added provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { type AppType } from "next/app";
+import { Provider } from "jotai";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
