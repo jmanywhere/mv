fbc72f36f4570690e5484f635678c228092cb6f4
Semi <>
2023-01-03 00:30:24 -0600

    prettyBN easier than all the other shit combined

---
+++ src/utils/bn.ts
@@ -0,0 +1,19 @@
+import type { BigNumber } from "ethers";
+import { commify, formatEther } from "ethers/lib/utils";
+
+export const prettyBN = (input: BigNumber, decimals?: number) => {
+
+  const stringResult = commify(formatEther(input))
+
+  if(decimals === 0)
+    return stringResult.split(".")[0]
+  if(typeof(decimals) == 'number' && decimals > 0){
+    const splitResult = stringResult.split(".")
+    if(!splitResult[1]) // ts made me do it
+      return splitResult[0]
+    splitResult[1] = splitResult[1].substring(0,decimals)
+    return splitResult.join(".")
+  }
+  
+  return stringResult
+}
