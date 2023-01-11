f791e24e84228a5f053fee5090606e9676603d89
Semi <>
2023-01-11 02:55:29 -0600

    if no address return null

--- src/hooks/useContracts.tsx
+++ src/hooks/useContracts.tsx
@@ -3,10 +3,14 @@ import { Contract, providers } from "ethers";
 import { useWeb3React } from "@web3-react/core";
 
 import SimpleETHRaise from "abi/SimpleCFETH.json";
+import MVRaise from "abi/MVRaise.json";
+import ERC20 from "abi/ERC20.json";
 import { getRpcUrl } from "data/chainData";
 
 const abiTypes = {
   SimpleETH: SimpleETHRaise,
+  MVRaise: MVRaise,
+  ERC20: ERC20,
 };
 
 const useContract = (
@@ -16,17 +20,19 @@ const useContract = (
 ) => {
   const { library } = useWeb3React();
   const baseLib = useMemo(() => {
+    if (chainId == 97) return library;
     return new providers.StaticJsonRpcProvider(getRpcUrl(chainId));
-  }, [chainId]);
+  }, [chainId, library]);
   // TODO cross chain implementations, get the static rpc for the respective chain.
   const reader: null | Contract = useMemo(() => {
+    if (!address) return null;
     return new Contract(address, abiTypes[type].abi, baseLib);
   }, [address, type, baseLib]);
 
   // IF USER CHANGES THEIR CHAINID, MAKE SURE TO ASK THE IDIOT TO CHANGE IT BACK TO THE APPROPRIATE CHAINID BEFORE FUCKUPS OCCUR
 
   const writer: null | Contract = useMemo(() => {
-    if (!library) return null;
+    if (!library || !address) return null;
     return new Contract(address, abiTypes[type].abi, library.getSigner());
   }, [library, address, type]);
 
