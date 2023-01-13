2d14d279abe7c6993f2090677dabf489d6046afc
Semi <>
2023-01-12 20:12:45 -0600

    description requires 1 condition per meta tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,8 +19,13 @@ const Layout = (props: {
   title: string;
   hideHeader?: boolean;
   simpleFooter?: boolean;
+  meta?: {
+    url?: string;
+    description?: string;
+    image?: string;
+  };
 }) => {
-  const { title, children, hideHeader, simpleFooter } = props;
+  const { title, children, hideHeader, simpleFooter, meta } = props;
   const raiseStyles = useAtomValue(raiseBasic);
   return (
     <div
@@ -32,6 +37,32 @@ const Layout = (props: {
     >
       <Head>
         <title>{title}</title>
+        <meta property="twitter:title" content={title} />
+        <meta name="og:title" content={title} />
+        {meta && (
+          <>
+            {meta.url && <meta name="og:url" content={meta.url} />}
+            {meta.description && (
+              <meta name="description" content={meta.description} />
+            )}
+            {meta.description && (
+              <meta property="og:description" content={meta.description} />
+            )}
+            {meta.description && (
+              <meta property="twitter:description" content={meta.description} />
+            )}
+            <meta
+              property="og:image"
+              content={meta.image || "https://moonvector.io/logo/mainLogo.png"}
+            />
+            <meta
+              property="twitter:image"
+              content={meta.image || "https://moonvector.io/logo/mainLogo.png"}
+            />
+            <meta property="twitter:card" content="summary" />
+            <meta property="twitter:site" content="@MoonVector_" />
+          </>
+        )}
       </Head>
       <Header price={100000000} hideHeader={hideHeader} />
       <main>
