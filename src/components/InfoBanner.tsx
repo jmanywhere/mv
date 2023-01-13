8d6b964673a39d0a1c8b92682ef944aecbc4dc66
Semi <>
2023-01-12 20:16:02 -0600

    added content

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -3,32 +3,12 @@ import React from "react";
 import Image from "next/image";
 //mv
 import img from "./../../public/images/Big Moon.png"; //Cambiar la imagen por la real
+import classNames from "classnames";
 
 const InfoBanner = () => {
   return (
-    <section
-      className="
-                
-                flex
-                w-full
-                flex-col
-                items-center
-                pt-[64px]
-                pb-[76px]
-                lg:flex-row
-                lg:items-start
-                "
-    >
-      <div
-        className="
-                    mb-[63px]
-                    flex
-                    flex-col
-                    items-center
-                    lg:mb-0
-                    lg:mr-[200px]
-      "
-      >
+    <section className=" flex w-full flex-col items-center pt-[64px] pb-[76px] lg:flex-row lg:items-start">
+      <div className="mb-[63px] flex flex-col items-center lg:mb-0 lg:mr-[200px]">
         <h2
           className=" 
                   mb-[58px]
@@ -59,14 +39,16 @@ const InfoBanner = () => {
                   mb-[35px]
                   max-w-[294px]
                   text-center
-                  text-lg
+                  text-lg font-bold 
                   text-white
-                  lg:max-w-[509px]
+                  lg:max-w-[calc(100%-40%)]
                   lg:text-left
-                  lg:font-bold
                 "
         >
-          Under Construction
+          Moon Vector is a new decentralized Private Sale, Public Sale, and
+          Crowd Funding platform that allows projects to seamlessly raise funds,
+          with or without having a token, allowing you the option of Crypto, or
+          traditional non-crypto, crowdfunding and fundraising.
         </p>
         <p
           className=" 
@@ -75,11 +57,19 @@ const InfoBanner = () => {
                   text-center
                   text-lg
                 text-white
-                lg:max-w-[509px]
+                lg:max-w-[calc(100%-40%)]
                 lg:text-left
                 "
         >
-          Please come back soon
+          Whether launching a private sale for your P2E Game IDO, launching a
+          royalty sharing fundraise to launch your non-crypto based video game,
+          raising funds for your brick and mortar retail store, or just
+          crowdfunding some relief to help pay for your cute little kitty
+          cat&apos;s vet bill, Moon Vector has you covered. Easy to deploy, easy
+          to set your terms and timeline, and easy to collect and distribute
+          funds/tokens, Moon Vector removes the friction and headache from
+          launching projects and jumping through hoops. Just set your terms,
+          customize your page, and you&apos;re ready to go!
         </p>
         <div
           className="
@@ -89,30 +79,18 @@ const InfoBanner = () => {
                   lg:flex-row
         "
         >
-          <button
-            className=" 
-                  mb-[39px]
-                  max-w-[175px]
-                  rounded-xl
-                  bg-white
-                  px-[41px]
-                  py-[20px]
-                  text-center
-                  text-base
-                  lg:mb-0
-                  lg:mr-[51px]
-                  "
+          <a
+            href="https://medium.com/@admin_77189/introducing-moon-vector-d0f65fc533b9"
+            target="_blank"
+            rel="noreferrer"
+            className={classNames(
+              "mb-[39px] max-w-[175px] rounded-xl bg-white px-[41px] py-[20px] text-center text-base lg:mb-0 lg:mr-[51px]",
+              "hover:bg-primary hover:text-white"
+            )}
           >
             Read More
-          </button>
-          <a
-            className=" 
-                text-center
-                text-base
-                text-white
-                underline
-                underline-offset-4"
-          >
+          </a>
+          <a className="cursor-default text-center text-base text-t_dark underline underline-offset-4">
             Explore Projects
           </a>
         </div>
