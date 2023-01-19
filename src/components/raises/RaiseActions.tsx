37de25aa14667696e154647d947403732c03e611
Semi <>
2023-01-18 23:18:01 -0600

    conditional render to Back button and execute action on Next

--- src/components/raises/RaiseActions.tsx
+++ src/components/raises/RaiseActions.tsx
@@ -1,24 +1,34 @@
-import { useSetAtom } from "jotai";
+import { useAtom } from "jotai";
 import { stepAtom } from "data/raiseAtoms";
+import classNames from "classnames";
 
-const RaiseActions = (props: { disableNext: boolean }) => {
-  const { disableNext } = props;
-  const setStep = useSetAtom(stepAtom);
+const RaiseActions = (props: { disableNext: boolean; action: () => void }) => {
+  const { disableNext, action } = props;
+  const [step, setStep] = useAtom(stepAtom);
   const scrollTop = (fwd: boolean) => {
     window.scrollTo({ top: 60, behavior: "smooth" });
     setStep((s) => (fwd ? s + 1 : s - 1));
   };
   return (
     <div className="flex flex-row justify-between pt-6">
-      <button
-        className="rounded-lg bg-slate-300 px-4 py-2 text-black hover:bg-white"
-        onClick={() => scrollTop(false)}
-      >
-        Back
-      </button>
+      {step == 1 ? (
+        <div />
+      ) : (
+        <button
+          className={classNames(
+            "rounded-lg bg-slate-300 px-4 py-2 text-black hover:bg-white"
+          )}
+          onClick={() => scrollTop(false)}
+        >
+          Back
+        </button>
+      )}
       <button
         className="rounded-lg bg-primary px-4 py-2 disabled:bg-slate-600 disabled:text-t_dark"
-        onClick={() => scrollTop(true)}
+        onClick={() => {
+          action();
+          scrollTop(true);
+        }}
         disabled={disableNext}
       >
         Next
