6acb2dc5d24682abbd9d3906100d94ef2faabc9a
Semi <>
2023-01-19 00:27:46 -0600

    type submit

--- src/components/raises/RaiseActions.tsx
+++ src/components/raises/RaiseActions.tsx
@@ -24,6 +24,7 @@ const RaiseActions = (props: { disableNext: boolean; action: () => void }) => {
         </button>
       )}
       <button
+        type="submit"
         className="rounded-lg bg-primary px-4 py-2 disabled:bg-slate-600 disabled:text-t_dark"
         onClick={() => {
           action();
