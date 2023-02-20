5d7e47a39a9dad60b856f2c132bb50b80cd03e80
Semi <>
2023-02-20 12:34:02 -0600

    setup wagmi and web3modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,38 @@ import { Provider } from "jotai";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { bsc } from "@wagmi/chains";
+
 import { Web3ReactProvider } from "@web3-react/core";
 import { providers } from "ethers";
+
+if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
+}
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const chains = [bsc];
+
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    version: "2",
+    appName: "unicus",
+    chains,
+    projectId,
+  }),
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const getLibrary = (provider: any): providers.Web3Provider => {
@@ -16,9 +46,18 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
   return (
     <Provider>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Component {...pageProps} />
-      </Web3ReactProvider>
+      <WagmiConfig client={wagmiClient}>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <Component {...pageProps} />
+        </Web3ReactProvider>
+      </WagmiConfig>
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeMode="dark"
+        themeColor="default"
+        themeBackground="gradient"
+      />
     </Provider>
   );
 };
