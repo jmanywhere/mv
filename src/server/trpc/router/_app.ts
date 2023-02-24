c36016c0ded8868056f71241770b23634e1c17a8
Semi <>
2023-02-23 20:00:19 -0600

    added new router to trpc

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { exampleRouter } from "./example";
+import { mediaRouter } from "./media";
 
 export const appRouter = router({
   example: exampleRouter,
+  media: mediaRouter,
 });
 
 // export type definition of API
