a64762b65dd6e0737775468f71d08b7ca8bad9ac
Semi <>
2023-02-24 14:12:04 -0600

    start of raise router api

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { exampleRouter } from "./example";
 import { mediaRouter } from "./media";
+import { raiseRouter } from "./raise";
 
 export const appRouter = router({
   example: exampleRouter,
   media: mediaRouter,
+  raise: raiseRouter
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/raise.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+
+export const raiseRouter = router({
+  customPathCheck: publicProcedure
+    .input(z.string().min(3).max(15).transform( t => t.toLowerCase() ))
+    .query(async ({ ctx, input }) => {
+      return (await ctx.prisma.upsell.count({
+        where: {
+          routeName: input
+        }
+      })) > 0
+    })
+})
