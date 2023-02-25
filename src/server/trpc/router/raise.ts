d145dfd9ce10b3e8f17714d86e11855607d69d45
Semi <>
2023-02-25 11:08:13 -0600

    check that addresses are not input

--- src/server/trpc/router/raise.ts
+++ src/server/trpc/router/raise.ts
@@ -1,11 +1,17 @@
 import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
+import { isAddress } from "ethers/lib/utils.js";
 
 export const raiseRouter = router({
   customPathCheck: publicProcedure
     .input(z.string().min(3).max(15).transform( t => t.toLowerCase() ))
     .query(async ({ ctx, input }) => {
+      if(isAddress(input))
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Address is not a valid route name"
+        })
       return (await ctx.prisma.upsell.count({
         where: {
           routeName: {
