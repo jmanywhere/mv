f8c16adf4c4420b6b7fe0c6ebd9bb91dd1466fbb
Semi <>
2023-05-10 01:41:33 -0600

    remove Trpc, because it might be breaking shit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import { type AppProps } from "next/app";
 
-import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default trpc.withTRPC(MyApp);
+export default MyApp;
